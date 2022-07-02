e0da8fc6ad097d7f72a7ca944d8e0138196747c3
Ivan Lanuza <icdlanuza@gmail.com>
2022-07-03 07:11:20 +0800

    Full Code

--- pages/profile/[id].js
+++ pages/profile/[id].js
@@ -1,12 +1,12 @@
 import Head from "next/head";
 import Link from "next/link";
 
-import { getProducts } from "lib/data";
 import prisma from "lib/prisma";
+import { getProducts, getUser } from "lib/data";
 
 import Heading from "components/Heading";
 
-export default function Home({ products }) {
+export default function Profile({ user, products }) {
   return (
     <div>
       <Head>
@@ -18,7 +18,7 @@ export default function Home({ products }) {
       <Heading />
 
       <h1 className="flex justify-center mt-20 text-xl">
-        Explore the most popular products
+        Products made by {user.name}
       </h1>
 
       <div className="flex justify-center mt-10">
@@ -58,11 +58,15 @@ export default function Home({ products }) {
 }
 
 export async function getServerSideProps(context) {
-  let products = await getProducts({ take: 3 }, prisma);
+  let user = await getUser(context.params.id, prisma);
+  user = JSON.parse(JSON.stringify(user));
+
+  let products = await getProducts({ author: context.params.id }, prisma);
   products = JSON.parse(JSON.stringify(products));
 
   return {
     props: {
+      user,
       products,
     },
   };
